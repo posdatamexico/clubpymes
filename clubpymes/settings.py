fe98dc12e275f59e799a44a76f3dd6f0391e414c
posdatamexico <76191901+posdatamexico@users.noreply.github.com>
2021-04-15 10:56:11 -0700

    Update settings.py

--- clubpymes/settings.py
+++ clubpymes/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z31x-ac-+icp0v)*m&b7yx0+q^(_60oa4&k=s(&vbif!84_=lp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
